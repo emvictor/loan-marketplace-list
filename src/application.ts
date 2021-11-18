d154bb398388dab95dab2b94c439cf53738dd4ce
emvictor <emivalvictor@hotmail.com>
2021-11-18 18:22:30 -0300

    first commit

---
+++ src/application.ts
@@ -0,0 +1,39 @@
+import { Category, Loan, Investment, MarketplaceItem } from "./types";
+
+export const generateMarketplaceList = (
+  loanList: Loan[],
+  investmentList: Investment[]
+): MarketplaceItem[] => {
+  const marketplaceList: MarketplaceItem[] = loanList.map((loan) => ({
+    id: loan.id,
+    category: loan.category,
+    expiresAt: loan.expiresAt,
+    totalRequestedAmount: loan.totalRequestedAmountCents / 100,
+    totalInvestmentAmount: investmentList.reduce((acc, investment) => {
+      if (investment.loanId === loan.id) {
+        return investment.totalInvestedAmountCents / 100 + acc;
+      }
+      return acc;
+    }, 0),
+  }));
+  return marketplaceList.sort(comparator);
+};
+
+const categoryWeight: Record<Category, number> = {
+  [Category.Y]: 1,
+  [Category.X]: 2,
+  [Category.Z]: 3,
+};
+
+const comparator = (itemA: MarketplaceItem, itemB: MarketplaceItem): number => {
+  if (categoryWeight[itemA.category] < categoryWeight[itemB.category]) {
+    return 1;
+  }
+  if (categoryWeight[itemB.category] < categoryWeight[itemA.category]) {
+    return -1;
+  }
+  if (itemA.expiresAt < itemB.expiresAt) {
+    return -1;
+  }
+  return 1;
+};
